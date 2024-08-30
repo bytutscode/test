e900b19a694c26d6f2431485bf3c7103bb649e11
Samuel <mielszrx@gmail.com>
2024-08-30 08:45:05 -0300

    fix(index.ts): support 10mb payload size and json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes';
+import path from 'path';
+
 
 dotenv.config();
 
 dotenv.config();
 
 const app = express();
+app.use(express.json( {limit: '10mb'} ));
+app.use(express.static(path.join(__dirname, 'temp_uploads')));
 
 app.use(router);
 
